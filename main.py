6beb0848e753fa9d9c12aea761baa21620bb6b45
VenturaVini <110046348+VenturaVini@users.noreply.github.com>
2025-06-27 13:53:55 +0000

    teste2

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
+from time import sleep
 
 def main():
     print("🔁 Iniciando teste Python...")
     response = requests.get("https://httpbin.org/get")
     print("✅ Status Code:", response.status_code)
+    print("📦 Dados:", response.json())
+    sleep(15)
     print("📦 Dados:", response.json())
 
 if __name__ == "__main__":
