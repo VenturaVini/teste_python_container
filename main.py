aa94dbb8360628ff45aa99443c068131b3e7e635
VenturaVini <110046348+VenturaVini@users.noreply.github.com>
2025-06-27 14:22:41 +0000

    .

--- main.py
+++ main.py
@@ -4,22 +4,6 @@ from services.telegram import enviar_mensagem
 
 def main():
     print("🔁 Iniciando teste Python...")
-    response = requests.get("https://httpbin.org/get")
-    print("✅ Status Code:", response.status_code)
-    print("📦 Dados:", response.json())
-    sleep(15)
-    print("📦 Dados:", response.json())
-    enviar_mensagem('Telegram Funcionando!!')
-
-
-if __name__ == "__main__":
-    main()
-import requests
-from time import sleep
-from services.telegram import enviar_mensagem
-
-def main():
-    print("🔁 Iniciando teste Python...")
     
     try:
         response = requests.get("https://httpbin.org/get", timeout=10)
